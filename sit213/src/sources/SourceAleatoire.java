11ce42f251a315909b583049f1e73f8233ecd671
Simon ROUX <simon@MSI>
2022-09-09 12:08:03 +0200

    create src

---
+++ sit213/src/sources/SourceAleatoire.java
@@ -0,0 +1,66 @@
+package sources;
+import java.util.Random;
+
+import information.Information;
+
+/**
+ * Une source qui envoie un message aleatoire
+ */
+public class SourceAleatoire extends Source<Boolean> {
+
+	/**
+	 * Constructeur d'une source aleatoire.
+	 * Elle envoie un message de longueur 7 en booléens.
+	 */
+    public SourceAleatoire () {
+        informationGeneree = new Information<Boolean>();
+        Random random = new Random();
+        for (int i = 0; i < 7; i++) {
+        	if (random.nextDouble(1) > 0.5) {
+        		informationGeneree.add(true);
+        	} else {
+        		informationGeneree.add(false);
+        	}
+        }
+    }
+
+    /**
+     * Constructeur d'une source aléatoire personnalisable.
+     * Elle envoie un message de longueur personnalisée en booléens.^
+     * 0 et 1 sont équiprobables.
+     * 
+     * @param l - Longueur du message binaire à envoyer
+     */
+    public SourceAleatoire (int l) {
+        informationGeneree = new Information<Boolean>();
+        Random random = new Random();
+        for (int i = 0; i < l; i++) {
+        	if (random.nextDouble(1) > 0.5) {
+        		informationGeneree.add(true);
+        	} else {
+        		informationGeneree.add(false);
+        	}
+        }
+    }
+
+    /**
+     * Constructeur d'une source aléatoire personnalisable supportant une graine de génération.
+     * Elle envoie un message de longueur personnalisée à partir d'une graine personnalisée en booléens.
+     * C'est la graine qui définit les probabilités des symboles.
+     * 
+     * @param l - Longueur du message binaine à envoyer
+     * @param seed - Graine pour l'aléatoire
+     */
+    public SourceAleatoire (int l, int seed) {
+        informationGeneree = new Information<Boolean>();
+        Random random = new Random(seed);
+        for (int i = 0; i < l; i++) {
+        	if (random.nextDouble(1) > 0.5) {
+        		informationGeneree.add(true);
+        	} else {
+        		informationGeneree.add(false);
+        	}
+        }
+    }
+
+}
