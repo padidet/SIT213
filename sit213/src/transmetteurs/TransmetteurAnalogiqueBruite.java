ef059f1c95966ba76fe26ae7d99b54494b974294
Simon ROUX <simon@MSI>
2022-09-16 11:56:59 +0200

    add class

---
+++ sit213/src/transmetteurs/TransmetteurAnalogiqueBruite.java
@@ -0,0 +1,25 @@
+package transmetteurs;
+
+import information.Information;
+import information.InformationNonConformeException;
+
+public class TransmetteurAnalogiqueBruite extends Transmetteur<Float,Float>{
+	
+	public TransmetteurAnalogiqueBruite() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	@Override
+	public void emettre() throws InformationNonConformeException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConformeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
