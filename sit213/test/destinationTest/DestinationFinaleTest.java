d389ceeba9fd6a183cad4866155c9d5004e71e3b
Simon ROUX <simon@MSI>
2022-09-22 00:12:44 +0200

    create destinationFinaleTest

--- sit213/test/destinationTest/DestinationFinaleTest.java
+++ sit213/test/destinationTest/DestinationFinaleTest.java
@@ -1,5 +1,9 @@
 package destinationTest;
 
+import information.*;
+import destinations.*;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +11,19 @@ import org.junit.jupiter.api.Test;
 class DestinationFinaleTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testRecevoir() {
+        System.out.println("Test recevoir");
+        Boolean bits[] = {true, false, true};
+        Information<Boolean> information = new Information<>(bits);
+        DestinationFinale instance = new DestinationFinale();
+        try {
+            instance.recevoir(information);
+        } catch (InformationNonConformeException e) {
+            fail("The test fail because recevoir didn't accept bool array.");
+        }
+        Information<Boolean> informationRecue = instance.getInformationRecue();
+
+        assertEquals(informationRecue, information);
+    }
 
 }
